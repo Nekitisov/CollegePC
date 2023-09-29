eefb25d466218375cbbbb5ed34af9e95ecf7e54d
Nekitisov <nekitisov74@gmail.com>
2023-09-29 14:08:02 +0300

    add js script

---
+++ script.js
@@ -0,0 +1,5 @@
+let tb = document.getElementById('age-table');
+let labels = document.querySelectorAll('td > label');
+let tr = document.querySelector('td')
+let td = tr.firstChild
+console.log(tb, labels, td)
